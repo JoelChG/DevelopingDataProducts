3725d3d4415e90f2a74d56606759aab1ec35caa6
JoelChG <chavez.joel@uabc.edu.mx>
2022-02-04 21:01:01 -0800

    Fixed some mistakes and added a paragraph to shiny app

--- ShinyApp-code/ui.R
+++ ShinyApp-code/ui.R
@@ -108,13 +108,18 @@ shinyUI(fluidPage(
             tableOutput('confusionMatrix'), 
             tags$p("And the results of the model test statistics are displayed
                    below:"), 
-            tableOutput('confusionMatrix2'), 
+            tableOutput('confusionMatrix2'),
+            tags$p("Even though the model apparently had an excellent sensitivity
+                   and specificity on the predictions for the testing data set, 
+                   it is important to note that the model is most likely  overfitted, 
+                   and that the predictions for Out of Sample observations may
+                   most likely not be accurate at all.")
             tags$h3("Disclaimer"), 
             tags$p("This is for demonstration purposes only, this is not a 
                    diagnostic tool and cannot be used to confirm or exclude 
                    a diagnosis."), 
             tags$h3("See more"), 
-            tags$p("To see more about the developmet of this app, click the
+            tags$p("To see more about the development of this app, click the
                    links below"),
             tags$a(href="https://github.com/JoelChG/DevelopingDataProducts",
                    "Project GitHub Repository"), 
